48e9b0f9381c8da0a380c44938b0d3f6d2298dd0
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2024-07-15 16:57:55 +0500

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -25,7 +25,7 @@ export default function Document() {
             `,
           }}
         />
-        <noscript><div><img src="https://mc.yandex.ru/watch/90159099" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript><div><img src="https://mc.yandex.ru/watch/90159099" alt="" /></div></noscript>
       </body>
     </Html>
   );
